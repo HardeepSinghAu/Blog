a539a909e8b9fde4769a3e7647485b6d1cc616d0
HardeepSinghAu <11919@coderacademy.edu.au>
2022-05-11 19:34:25 +1000

    CRUD for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,16 @@
 class ArticlesController < ApplicationController
     def index
         @time = Time.now
-        @articles = Article.all
+        @articles = Article.all 
+    end
+
+    def new
+        @articles = Article.new
+    end
+    
+    def create
+        @articles = Article.new(title: "How to prepare for an interview", body: "Every interview is an opportuniry to prepare.", importance: 6)
+        @articles.save
     end
 
 end
